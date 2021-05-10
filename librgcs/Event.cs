c51f406cb241ee04427fc6e144014c6d7043fd02
Derailedzack <36719411+Derailedzack@users.noreply.github.com>
2021-05-10 07:43:05 -0400

    Added Event.cs

---
+++ librgcs/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rgcs
+{
+    public class Event
+    {
+        public void SetEventHandler(World world,int id,Action<World,Event> handler)
+        {
+
+        }
+    }
+}
