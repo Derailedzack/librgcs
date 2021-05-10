55acec4c31da9ae95cecf623c0b4716a1a26ba6e
Derailedzack <36719411+Derailedzack@users.noreply.github.com>
2021-05-10 07:45:34 -0400

    Create Vector3.cs.

---
+++ librgcs/Vector3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rgcs
+{
+    public class Vector3<T>
+    {
+      
+        T X;
+        T Y;
+        T Z;
+
+        public Vector3(T x, T y, T z)
+        {
+            x = X;
+            y = Y;
+            z = Z;
+        }
+        public T GetX()
+        {
+            return X;
+        }
+        public T GetY()
+        {
+            return Y;
+        }
+        public T GetZ()
+        {
+            return Z;
+        }
+        public void SetX(T NewX)
+        {
+            X = NewX;
+        }
+        public void SetY(T NewY)
+        {
+            Y = NewY;
+        }
+        public void SetZ(T NewZ) {
+            Z = NewZ;
+        }
+
+        ~Vector3()
+        {
+           
+        }
+    }
+}
